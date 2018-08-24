11e00928c83ef2b433d313c857b7a5a7e0cd3e1e
cc1072027237 <1072027237@qq.com>
2018-08-24 15:24:43 +0800

    release

--- release/build-xsd.bat
+++ release/build-xsd.bat
@@ -1,6 +1,5 @@
 @echo off
 color 0a
-chcp 65001
 
 echo 此脚本自动生成当前目录下所有xview文件的xsd文件
 echo 建议：将此脚本放在项目目录下运行，项目目录或它的子目录下须有io.xview.jar
@@ -11,5 +10,5 @@ for /f "delims=" %%i in ('dir  /b/a-d/s *.jar') do (
     set CLASSPATH=!CLASSPATH!;%%i
 )
 
-java -Dfile.encoding=UTF-8 -classpath "%CLASSPATH%" io.xview.XViewXml
+java -classpath "%CLASSPATH%" -Dfile.encoding=UTF-8 io.xview.XViewXml
 pause
