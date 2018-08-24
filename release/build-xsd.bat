49c490493802565399741c3faa8d054bfc701cbe
cc1072027237 <1072027237@qq.com>
2018-08-24 15:45:29 +0800

    release

--- release/build-xsd.bat
+++ release/build-xsd.bat
@@ -1,8 +1,4 @@
 @echo off
-color 0a
-
-echo 此脚本自动生成当前目录下所有xview文件的xsd文件
-echo 建议：将此脚本放在项目目录下运行，项目目录或它的子目录下须有io.xview.jar
 
 SETLOCAL ENABLEDELAYEDEXPANSION
 set CLASSPATH=.
@@ -11,4 +7,3 @@ for /f "delims=" %%i in ('dir  /b/a-d/s *.jar') do (
 )
 
 java -classpath "%CLASSPATH%" -Dfile.encoding=UTF-8 io.xview.XViewXml
-pause
